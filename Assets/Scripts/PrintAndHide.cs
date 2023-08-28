cf1aecfc953c5b2a16b035b628049decaf2943f2
Thanh Nhan <85187276+NhanVo-gmt@users.noreply.github.com>
2023-08-28 15:32:50 +1000

    100

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,11 +6,37 @@ public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend;
     private int num = 3;
+    private int maxNum;
+
+    void Start()
+    {
+        if (gameObject.tag == "Red")
+        {
+            maxNum = 100;
+        }
+        
+        if (gameObject.tag == "Blue")
+        {
+            maxNum = Random.Range(150, 251);
+        }
+    }
     
     // Update is called once per frame
     void Update()
     {
         num++;
         Debug.Log($"{gameObject.name}:{num}");
+        if (num > maxNum)
+        {
+            if (gameObject.tag == "Red")
+            {
+                gameObject.SetActive(false);
+                
+            }
+            else
+            {
+                rend.enabled = false;
+            }
+        }
     }
 }
